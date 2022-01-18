f276f46de8d3391cfda89d8d636e975b8984b28e
Tolu Thomas <toluwasethomas1@gmail.com>
2022-01-18 10:27:51 +0100

    setting up db.go

--- database/db.go
+++ database/db.go
@@ -1,1 +1,38 @@
 package database
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/toluwase1/golang-inventory-system/models"
+	"log"
+)
+
+/*DB is connected database object*/
+var DB *gorm.DB
+
+func Setup() {
+	host := "host"
+	port := "port"
+	dbname := "dbname"
+	user := "user"
+	password := "password"
+
+	db, err := gorm.Open("postgres",
+		"host="+host+
+			" port="+port+
+			" user="+user+
+			" dbname="+dbname+
+			" sslmode=disable password="+password)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.LogMode(false)
+	db.AutoMigrate([]models.Inventory{})
+	DB = db
+}
+
+// GetDB helps you to get a connection
+func GetDB() *gorm.DB {
+	return DB
+}
