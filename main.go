37b1293fa3a85c071f381d2321fdee9638663611
Tolu Thomas <toluwasethomas1@gmail.com>
2022-01-20 04:23:50 +0100

    main.go

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/toluwase1/golang-inventory-system/database"
 	"github.com/toluwase1/golang-inventory-system/routers"
 	"log"
+	"os"
 )
 
 func main() {
 	database.Setup()
+	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	if port == ":" {
+		port = ":8080"
+	}
 	r := routers.Setup()
 	if err := r.Run("127.0.0.1:8080"); err != nil {
 		log.Fatal(err)
 	}
+
 }
